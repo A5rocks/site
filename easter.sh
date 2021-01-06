63b84d69be54962d80ee8a1ed86a962279f55533
A5rocks <git@helvetica.moe>
2021-01-06 11:57:15 +0900

    shhhhhh

---
+++ easter.sh
@@ -0,0 +1,4 @@
+echo 'Hello, I see you might have seen this. '
+echo 'If you were on helvetica.moe/_logs, '
+echo 'maybe check out github.com/A5rocks/site?'
+echo 'And vice versa, for those on github.'
