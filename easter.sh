c3757232960d5b8870b3f22ad0f427720623f98e
A5rocks <git@helvetica.moe>
2022-09-05 07:36:52 +0900

    Remove easter egg

--- easter.sh
+++
@@ -1,4 +0,0 @@
-echo 'Hello, I see you might have seen this. '
-echo 'If you were on helvetica.moe/_logs, '
-echo 'maybe check out github.com/A5rocks/site?'
-echo 'And vice versa, for those on github.'
