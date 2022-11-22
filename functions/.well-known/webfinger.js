cb26809bfe72c9ec79cec0e5d0e394b2cab3f21d
A5rocks <git@helvetica.moe>
2022-11-22 13:26:02 +0900

    Clean up mastodon redirecting function

--- functions/.well-known/webfinger.js
+++ functions/.well-known/webfinger.js
@@ -1,9 +1,14 @@
 // lewd name!
 // https://docs.joinmastodon.org/spec/webfinger
-export async function onRequest({ env, request }) {
+
+const redirects = {
+	"acct:a5rocks@helvetica.moe": "acct:A5rocks@uwu.social"
+};
+
+export async function onRequest({ request }) {
 	if (request.method !== "GET") {
 		return new Response(
-			"This webfinger implementaiton only supports `GET` requests.",
+			"This webfinger implementation only supports `GET` requests.",
 			{ status: 405 }
 		);
 	}
@@ -13,31 +18,12 @@ export async function onRequest({ env, request }) {
 		return new Response("Missing `resource` query parameter.", { status: 400 });
 	}
 
-	if (resource.toLowerCase() !== "acct:a5rocks@helvetica.moe") {
+	if (!Object.keys(redirects).includes(resource.toLowerCase())) {
 		return new Response("That user is not known!", { status: 404 });
 	}
 
 	return new Response(JSON.stringify({
-		subject: "acct:A5rocks@uwu.social",
-		aliases: [
-			"https://uwu.social/@A5rocks",
-			"https://uwu.social/users/@A5rocks",
-		],
-		links: [
-			{
-				rel: "http://webfinger.net/rel/profile-page",
-				type: "text/html",
-				href: "https://uwu.social/@A5rocks",
-			},
-			{
-				rel: "self",
-				type: "application/activity+json",
-				href: "https://uwu.social/users/@A5rocks",
-			},
-			{
-				rel: "http://ostatus.org/schema/1.0/subscribe",
-				template: "https://uwu.social/authorize_interaction?uri={uri}",
-			},
-		],
+		// mastodon will treat this as a redirect
+		subject: redirects[resource.toLowerCase()],
 	}), { status: 200, headers: { "Content-Type": "application/json" } });
 }
